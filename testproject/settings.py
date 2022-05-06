ec4633f064757840fd59fd5db5b8691bb79e6863
gitacc908 <dylan9803@mail.ru>
2022-05-07 00:23:51 +0600

    updated

--- testproject/settings.py
+++ testproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-14m^^)jfmd*yjeq!)bn=58-9t@b)=su-pq7#w*mu9k^7a*gd#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
